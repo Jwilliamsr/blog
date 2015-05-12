2b5db91e8fa6e2596484f4b28bfdcae7540fae8d
Jwilliamsr <jessewilliams.r@gmail.com>
2015-05-12 08:25:52 -0700

    Factory button script for Android

---
+++ androidFactory.js
@@ -0,0 +1,6 @@
+<script type="text/javascript"
+        src="https://codenvy.com/factory/resources/factory.js"
+        style="advanced"
+        url="https://codenvy.com/factory?id=g08a3escw5kxj2xk"
+        logo="https://raw.githubusercontent.com/codenvy/factories/master/images/android.png">
+</script>
